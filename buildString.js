9267a08a62d7965d6ed0b4f735113bcc99a2b616
nakajima-satoru <nakajima@email.com>
2022-03-08 11:24:13 +0900

    3/8

--- buildString.js
+++ buildString.js
@@ -5,8 +5,8 @@ module.exports = function(){
     var syncStr = fs.readFileSync(__dirname + "/index.js").toString();
     syncStr = syncStr.split("module.exports = ")[1];
 
-    syncStr = syncStr.replace("Buffer.from(data).toString('base64');","btoa(encodeURIComponent(data));");
-    syncStr = syncStr.replace("Buffer.from(stringB64, 'base64').toString();","decodeURIComponent(atob(stringB64));");
+    syncStr = syncStr.replace("Buffer.from(data).toString('base64');","btoa(unescape(encodeURIComponent(data)));");
+    syncStr = syncStr.replace("Buffer.from(stringB64, 'base64').toString();","decodeURIComponent(escape(atob(stringB64)));");
 
     return syncStr;
 };
